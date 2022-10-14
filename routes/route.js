3b24c79d416c14ddd56cbba495126dfe2c528beb
Ravigan123 <65658766+Ravigan123@users.noreply.github.com>
2022-10-14 10:43:12 +0200

    Add files via upload

--- routes/route.js
+++ routes/route.js
@@ -1,41 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const DataController = require("../controllers/DataController");
-const DeviceController = require("../controllers/DeviceController");
-const LocationController = require("../controllers/LocationController");
-const TypeController = require("../controllers/TypeController");
-const AlertController = require("../controllers/AlertController");
-const ReceiverController = require("../controllers/ReceiverController");
 
-router.get("/", DataController.getAllData);
-router.get("/store", DataController.translateData);
-router.delete("/:id", DataController.deleteData);
+const UserController = require("../controllers/UserController");
 
-router.get("/type", TypeController.getAllTypes);
-router.get("/typeSelect", TypeController.getTypesToSelect);
-
-router.get("/device", DeviceController.getAllDevices);
-router.get("/deviceSelect", DeviceController.getDevicesToSelect);
-router.post("/device/", DeviceController.storeDevice);
-router.put("/device/:id", DeviceController.updateDevice);
-router.delete("/device/:id", DeviceController.deleteDevice);
-
-router.get("/location", LocationController.getAllLocations);
-router.get("/locationSelect", LocationController.getLocationsToSelect);
-router.post("/location/", LocationController.storeLocation);
-router.put("/location/:id", LocationController.updateLocation);
-router.delete("/location/:id", LocationController.deleteLocation);
-
-router.get("/receiver", ReceiverController.getAllReceivers);
-router.get("/receiverDetails/:id", ReceiverController.getReceiverDetails);
-router.post("/receiverDetails/", ReceiverController.storeReceiverDetails);
-router.delete("/receiverDetails/:id", ReceiverController.deleteReceiverDetails);
-router.post("/receiver/", ReceiverController.storeReceiver);
-router.put("/receiver/:id", ReceiverController.updateReceiver);
-router.delete("/receiver/:id", ReceiverController.deleteReceiver);
-
-router.get("/alert", AlertController.getAllAlerts);
-router.get("/alertType", AlertController.getAlertType);
-router.put("/alert/:id", AlertController.doneAlert);
+router.post("/register", UserController.registerUser);
+router.get("/confirm/:id:token", UserController.confirmUser);
+router.post("/forgot-password", UserController.sendForgotPassword);
+router.post("/reset-password/:id:token", UserController.changeForgotPassword);
+// router.put("/location/:id", LocationController.updateLocation);
+// router.delete("/location/:id", LocationController.deleteLocation);
 
 module.exports = router;
