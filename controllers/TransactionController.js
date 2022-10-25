841dc425cb720f8349d3654567e38f435d4f13f7
Ravigan123 <65658766+Ravigan123@users.noreply.github.com>
2022-10-25 08:00:32 +0200

    Add files via upload

--- controllers/TransactionController.js
+++ controllers/TransactionController.js
@@ -1,4 +1,4 @@
-const Transaction = require("../models/Transaction")
+const Transaction = require("../models/Transaction");
 
 class TransactionController {
 	async addTransaction(req, res) {
@@ -12,14 +12,14 @@ class TransactionController {
 		// 		.status(422)
 		// 		.json({ message: "The given device already exists" })
 
-		const id_user = parseInt(req.session.user)
-		const id_category = parseInt(req.body.category)
-		const name = req.body.name
-		const type = parseInt(req.body.type)
-		const sum = parseFloat(req.body.sum)
-		const date = req.body.date
+		const id_user = parseInt(req.session.user);
+		const id_category = parseInt(req.body.category);
+		const name = req.body.name;
+		const type = parseInt(req.body.type);
+		const sum = parseFloat(req.body.sum);
+		const date = req.body.date;
 
-		let newTransaction
+		let newTransaction;
 		try {
 			newTransaction = await Transaction.query().insert({
 				id_user,
@@ -28,95 +28,66 @@ class TransactionController {
 				type,
 				sum,
 				date,
-			})
+			});
 		} catch (err) {
-			return res.status(422).json({ message: err.message })
+			return res.status(422).json({ message: err.message });
 		}
 
-		res.status(201).json(newTransaction)
+		res.status(201).json(newTransaction);
 	}
 
 	async getAllTransaction(req, res) {
-		const transactions = await Transaction.query()
-			.select(
-				"transactions.name_transaction",
-				"categories.name_category",
-				"type",
-				"sum_transaction",
-				"date",
-				"transactions.created_at"
-			)
-			.innerJoin("categories", "categories.id", "transactions.id_category")
-			.where("id_user", req.session.user)
+		const d = new Date();
+		const lastMonthFirstDay = new Date(d.getFullYear(), d.getMonth() - 1, 1);
+		const thisMonthFirstDay = new Date(d.getFullYear(), d.getMonth(), 1);
 
-		res.status(200).json(transactions)
-	}
+		const course = await Transaction.query()
+			.select("id", "name_transaction", "date")
+			.where("id_user", 1)
+			.where("date", ">=", lastMonthFirstDay)
+			.where("date", "<", thisMonthFirstDay);
 
-	async getLastMonthTransaction(req, res) {
-		const date = new Date()
-		const firstDayOfMonth = new Date(date.getFullYear(), date.getMonth(), 1)
-		const lastDayOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0)
-		const b = Date.parse("2019-01-01T00:00:00.000Z")
-		const a = new Date(b)
-		console.log(a)
-		date.setHours(date.getHours() + 2)
-		console.log(date.toString())
-
-		firstDayOfMonth.setHours(firstDayOfMonth.getHours() + 2)
-		lastDayOfMonth.setHours(lastDayOfMonth.getHours() + 2)
-		console.log(firstDayOfMonth)
-		console.log(lastDayOfMonth)
-		const transactions = await Transaction.query()
-			.select(
-				"transactions.name_transaction",
-				"categories.name_category",
-				"type",
-				"sum_transaction",
-				"date",
-				"transactions.created_at"
-			)
-			.innerJoin("categories", "categories.id", "transactions.id_category")
-			.where("date", "<", lastDayOfMonth)
-			.where("date", ">", firstDayOfMonth)
-			.where("id_user", req.session.user)
-
-		res.status(200).json(transactions)
+		res.status(200).json(course);
+		// const d1 = new Date(year, monthIndex [, day [, hours [, minutes [, seconds [,
+		// milliseconds]]]]]);
+		// const d2 =new Date(value);
+		// const d3 = new Date(dateString);
 	}
 
 	async getOneTransaction(req, res) {
 		const transaction = await Transaction.query()
 			.select("name", "type", "sum", "date", "created_at")
 			.where("id_user", req.session.user)
-			.where("id", req.params["id"])
+			.where("id", req.params["id"]);
 
-		res.status(200).json(transaction)
+		res.status(200).json(transaction);
 	}
 
 	async updateTransaction(req, res) {
-		const id = req.params.id
-		const id_category = parseInt(req.body.category)
-		const name = req.body.name
-		const type = parseInt(req.body.type)
-		const sum = parseFloat(req.body.sum)
-		const date = req.body.date
+		const id = req.params.id;
+		const id_category = parseInt(req.body.category);
+		const name = req.body.name;
+		const type = parseInt(req.body.type);
+		const sum = parseFloat(req.body.sum);
+		const date = req.body.date;
 
-		let transaction
+		let transaction;
 		try {
 			const transaction = await Transaction.query()
 				.findById(id)
-				.patch({ id_category, name, type, sum, date })
+				.patch({ id_category, name, type, sum, date });
 		} catch (err) {
-			return res.status(422).json({ message: err.message })
+			return res.status(422).json({ message: err.message });
 		}
 
-		res.status(201).json(transaction)
+		res.status(201).json(transaction);
 	}
 
 	async deleteTransaction(req, res) {
-		const id = req.params.id
-		const transaction = await Transaction.query().deleteById(id)
-		res.sendStatus(204)
+		const id = req.params.id;
+		const transaction = await Transaction.query().deleteById(id);
+		res.sendStatus(204);
 	}
 }
 
-module.exports = new TransactionController()
+module.exports = new TransactionController();
--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,175 +1,177 @@
-const User = require("../models/User")
-const sendEmail = require("../sendEmail.js")
-const jwt = require("jsonwebtoken")
-const bcrypt = require("bcrypt")
+const User = require("../models/User");
+const sendEmail = require("../sendEmail.js");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
 
 class UserController {
 	async login(req, res) {
-		const { email, password } = req.body
+		const { email, password } = req.body;
 
 		const userCheck = await User.query()
 			.select("id", "password", "isActive")
-			.where("email", email)
+			.where("email", email);
 
 		if (userCheck.length === 0) {
-			return res.status(401).json({ message: "Błędny login lub hasło" })
+			return res.status(401).json({ message: "Błędny login lub hasło" });
 		}
 
 		const validPassword = await bcrypt.compare(
 			password,
 			userCheck[0]["password"]
-		)
+		);
 
 		if (!validPassword)
-			return res.status(401).json({ message: "Błędny login lub hasło" })
+			return res.status(401).json({ message: "Błędny login lub hasło" });
 
 		if (parseInt(userCheck[0]["isActive"]) === 0) {
-			return res.status(401).json({ message: "Konto nie aktywne" })
+			return res.status(401).json({ message: "Konto nie aktywne" });
 		}
 
-		req.session.user = userCheck[0]["id"]
-		console.log(req.session)
-		res.status(200).json({ message: "zalogowany" })
+		req.session.user = userCheck[0]["id"];
+		console.log(req.session);
+		res.status(200).json({ message: "zalogowany" });
 		// res.redirect("/")
 	}
 
 	async test(req, res) {
-		console.log(req.session)
-		res.send("test")
+		console.log(req.session);
+		res.send("test");
 	}
 	async logout(req, res) {
-		req.session.destroy()
+		req.session.destroy();
 		// res.send("wylogowany")
-		res.status(200).json({ message: "wylogowany" })
+		res.status(200).json({ message: "wylogowany" });
 		// res.redirect("/")
 	}
 
 	async registerUser(req, res) {
-		const { email, password } = req.body
+		const { email, password } = req.body;
 
 		const userCheck = await User.query()
 			.select("id", "email", "password")
-			.where("email", email)
+			.where("email", email);
 
 		if (userCheck.length !== 0) {
 			if (email === userCheck[0]["email"]) {
-				res.send("Użytkownik o podanym adresie e-mail juz istnieje")
-				return
+				res.send("Użytkownik o podanym adresie e-mail juz istnieje");
+				return;
 			}
 		}
 
-		const salt = await bcrypt.genSalt(10)
-		const hashPassword = await bcrypt.hash(password, salt)
+		const salt = await bcrypt.genSalt(10);
+		const hashPassword = await bcrypt.hash(password, salt);
 
-		let newUser
+		let newUser;
 		try {
 			newUser = await User.query().insert({
 				email,
 				password: hashPassword,
 				isActive: 0,
-			})
+			});
 		} catch (err) {
-			return res.status(422).json({ message: err.message })
+			return res.status(422).json({ message: err.message });
 		}
 
-		const user = await User.query().select("id").where("email", email)
+		const user = await User.query().select("id").where("email", email);
 
-		const id = user[0]["id"]
-		const secret = process.env.JWT_TOKEN + email
+		const id = user[0]["id"];
+		const secret = process.env.JWT_TOKEN + email;
 		const payload = {
 			email,
 			id,
-		}
+		};
 
-		const token = jwt.sign(payload, secret, { expiresIn: "10000s" })
-		const link = `http://localhost:4000/potwierdz/${id}${token}`
-		sendEmail(email, "Aktywuj konto", link)
-		res.send("send")
+		const token = jwt.sign(payload, secret, { expiresIn: "10000s" });
+		const link = `http://localhost:4000/potwierdz/${id}${token}`;
+		sendEmail(email, "Aktywuj konto", link);
+		res.send("send");
 	}
 
 	async confirmUser(req, res) {
-		const { id, token } = req.params
+		const { id, token } = req.params;
 
 		const user = await User.query()
 			.select("id", "email", "password")
-			.where("id", id)
+			.where("id", id);
 
 		if (parseInt(id) !== user[0]["id"]) {
-			res.send("Brak uzytkownika")
-			return
+			res.send("Brak uzytkownika");
+			return;
 		}
 
-		const email = user[0]["email"]
-		const secret = process.env.JWT_TOKEN + email
+		const email = user[0]["email"];
+		const secret = process.env.JWT_TOKEN + email;
 
 		try {
-			const payload = jwt.verify(token, secret)
-			const userConfirm = await User.query().findById(id).patch({ isActive: 1 })
+			const payload = jwt.verify(token, secret);
+			const userConfirm = await User.query()
+				.findById(id)
+				.patch({ isActive: 1 });
 
-			res.status(201).json(userConfirm)
+			res.status(201).json(userConfirm);
 			// przenies do logowania
 		} catch (error) {
-			console.log(error)
-			res.send(error.message)
+			console.log(error);
+			res.send(error.message);
 		}
 	}
 
 	async sendForgotPassword(req, res) {
-		const { email } = req.body
+		const { email } = req.body;
 
 		const user = await User.query()
 			.select("id", "email", "password")
-			.where("email", email)
+			.where("email", email);
 
 		if (email !== user[0]["email"]) {
-			res.send("Brak uzytkownika")
-			return
+			res.send("Brak uzytkownika");
+			return;
 		}
 
-		const id = user[0]["id"]
-		const secret = process.env.JWT_TOKEN + user[0]["password"]
+		const id = user[0]["id"];
+		const secret = process.env.JWT_TOKEN + user[0]["password"];
 		const payload = {
 			email,
 			id,
-		}
+		};
 
-		const token = jwt.sign(payload, secret, { expiresIn: "10m" })
-		const link = `http://localhost:4000/resetuj-haslo/${id}${token}`
-		sendEmail(email, "change password", link)
-		res.send("send")
+		const token = jwt.sign(payload, secret, { expiresIn: "10m" });
+		const link = `http://localhost:4000/resetuj-haslo/${id}${token}`;
+		sendEmail(email, "change password", link);
+		res.send("send");
 	}
 
 	async changeForgotPassword(req, res) {
-		const { id, token } = req.params
-		const { password, password2 } = req.body
-		console.log(password)
+		const { id, token } = req.params;
+		const { password, password2 } = req.body;
+		console.log(password);
 
 		const user = await User.query()
 			.select("id", "email", "password")
-			.where("id", id)
+			.where("id", id);
 
-		console.log(req.body)
+		console.log(req.body);
 
 		if (parseInt(id) !== user[0]["id"]) {
-			res.send("Brak uzytkownika")
-			return
+			res.send("Brak uzytkownika");
+			return;
 		}
 
-		const secret = process.env.JWT_TOKEN + user[0]["password"]
+		const secret = process.env.JWT_TOKEN + user[0]["password"];
 
-		const salt = await bcrypt.genSalt(10)
-		const hashPassword = await bcrypt.hash(password, salt)
+		const salt = await bcrypt.genSalt(10);
+		const hashPassword = await bcrypt.hash(password, salt);
 
 		try {
-			const payload = jwt.verify(token, secret)
+			const payload = jwt.verify(token, secret);
 			const userUpdate = await User.query()
 				.findById(id)
-				.patch({ password: hashPassword })
+				.patch({ password: hashPassword });
 
-			res.status(201).json(userUpdate)
+			res.status(201).json(userUpdate);
 		} catch (error) {
-			console.log(error)
-			res.send(error.message)
+			console.log(error);
+			res.send(error.message);
 		}
 	}
 
@@ -178,4 +180,4 @@ class UserController {
 	async deleteUser(req, res) {}
 }
 
-module.exports = new UserController()
+module.exports = new UserController();
